20fad439b2aa47537ed3b1130db4f5efaa05885b
hyhy200g <104736688+hyhy200g@users.noreply.github.com>
2024-01-03 21:24:04 -0500

    Figure5.R

--- Figure5.R
+++ Figure5.R
@@ -236,6 +236,7 @@ FeaturePlot(ODC, features = "Pdgfra", cols = c("lightgrey", "#8B7D6B")) + theme_
 FeaturePlot(ODC, features = "Plp1", cols = c("lightgrey", "#8B7D6B")) + theme_void()
 
 # figure5D
+## UMAP
 ODC@meta.data$Celltype <- factor(ODC@meta.data$Celltype, levels = c("ODC-1", "ODC-2", "ODC-3", "ODC-4", "ODC-5"))
 levels(ODC@meta.data$Celltype)
 DimPlot(ODC, cols = c("#E89242FF",
@@ -246,55 +247,9 @@ DimPlot(ODC, cols = c("#E89242FF",
         group.by = "Celltype") +
   theme_void() +
   theme(title = element_blank())
-ODC_mat <- as.matrix(ODC@assays[["RNA"]]@counts)
-ODC_result <- CytoTRACE(mat = ODC_mat)
-ODC_score <- as.data.frame(ODC_result["CytoTRACE"]) %>%
-  mutate(Cell = rownames(ODC_score))
-ODC_umap <- as.data.frame(ODC@reductions$umap@cell.embeddings) %>%
-  mutate(Cell = rownames(ODC_umap))
-ODC_df <- merge(ODC_umap, ODC_score, by = "Cell")
-ggplot(data = ODC_df, mapping = aes(x = UMAP_1, y = UMAP_2, color = CytoTRACE)) +
-  geom_point(size = 0.3) +
-  labs(x = "UMAP1",
-       y = "UMAP2") +
-  scale_color_viridis_c("CytoTRACE",
-                        option = "plasma") +
-  theme_bw() +
-  theme_void()
-
-# figure5E
-ODC_numi <- ODC[["RNA"]]@counts
-
-normalize <- function(x){
-  return(log(x/sum(x)*10000 + 1))
-}
-
-ODC_norm <- apply(ODC_numi, 2, normalize)
-
-ODC_expr <- data.frame(cell = colnames(ODC_norm),
-                       Pdgfra = ODC_norm["Pdgfra",],
-                       Plp1 = ODC_norm["Plp1",],
-                       group = ODC@meta.data$Celltype)
-ODC_expr$group <- factor(ODC_expr$group)
-levels(ODC_expr$group) <- c("ODC-1", "ODC-2", "ODC-3", "ODC-4", "ODC-5")
-
-ggplot(ODC_expr, aes(x=group, y=Plp1, fill=group)) +
-  geom_violin(scale = "width") +
-  theme_classic() +
-  theme(legend.position = "none") +
-  labs(y="Normalized UMI") +
-  stat_summary(fun = "mean", geom = "point", color="yellow", size=3) +
-  scale_fill_manual(values = c("brown", "darkgreen", "grey", "orange", "blue"))
-
-# figure5D monocle3 for ODC
-
-library(monocle3)
-library(tidyverse)
-library(ComplexHeatmap)
-library(circlize)
-library(RColorBrewer)
 
 
+##Pseudotime analysis using monocle3 
 #### prepare the input for monocle3 ###
 expression_matrix <- ODC@assays$RNA@counts
 cell_metadata <- ODC@meta.data
@@ -393,3 +348,32 @@ p <- plot_cells(newcds,
                   group_label_size=4,
                   cell_size=0.5)
 p
+
+
+# figure5E
+ODC_numi <- ODC[["RNA"]]@counts
+
+normalize <- function(x){
+  return(log(x/sum(x)*10000 + 1))
+}
+
+ODC_norm <- apply(ODC_numi, 2, normalize)
+
+ODC_expr <- data.frame(cell = colnames(ODC_norm),
+                       Pdgfra = ODC_norm["Pdgfra",],
+                       Plp1 = ODC_norm["Plp1",],
+                       group = ODC@meta.data$Celltype)
+ODC_expr$group <- factor(ODC_expr$group)
+levels(ODC_expr$group) <- c("ODC-1", "ODC-2", "ODC-3", "ODC-4", "ODC-5")
+
+ggplot(ODC_expr, aes(x=group, y=Plp1, fill=group)) +
+  geom_violin(scale = "width") +
+  theme_classic() +
+  theme(legend.position = "none") +
+  labs(y="Normalized UMI") +
+  stat_summary(fun = "mean", geom = "point", color="yellow", size=3) +
+  scale_fill_manual(values = c("#E89242FF",
+                      "#fdcf9e",
+                      "lightgreen",
+                      "#4f8c9d",
+                      "#4e659b"))
